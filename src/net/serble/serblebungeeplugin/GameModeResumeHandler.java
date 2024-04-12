902968b4a0ea9fb1e552c7197e7bf83640a92cb3
copokbl <copokbl@serble.net>
2024-04-12 14:26:58 +1000

    Fix bungee disconnect loop bug

--- src/net/serble/serblebungeeplugin/GameModeResumeHandler.java
+++ src/net/serble/serblebungeeplugin/GameModeResumeHandler.java
@@ -1,12 +1,16 @@
 package net.serble.serblebungeeplugin;
 
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
-import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.event.ServerConnectEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
+import net.serble.serblebungeeplugin.Schemas.Config.GameMode;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class GameModeResumeHandler implements Listener {
     private final YmlConfig previousGameModes;
@@ -20,10 +24,33 @@ public class GameModeResumeHandler implements Listener {
     }
 
     @EventHandler
-    public void onJoin(PostLoginEvent e) {
+    public void onJoin(ServerConnectEvent e) {
+        // Check if it is the first time joining
+        if (e.getReason() != ServerConnectEvent.Reason.JOIN_PROXY) {
+            return;
+        }
+
         ProxiedPlayer p = e.getPlayer();
 
+        if (!p.isConnected()) {
+            Main.plugin.getLogger().info("Could not resume session for: " + p.getName() + " (Not connected)");
+            return;
+        }
+
         String previousGameMode = previousGameModes.getConfiguration().getString(p.getUniqueId().toString(), ConfigUtil.configuration.getString("hub-gamemode"));
+
+        Optional<GameMode> prevGmObj = Main.jsonConfig.GameModes
+                .stream()
+                .filter(gm -> gm.Name.equalsIgnoreCase(previousGameMode)).findFirst();
+
+        if (!prevGmObj.isPresent()) {
+            Main.plugin.getLogger().severe("Could not resume session for: " + p.getName() + " (Game mode not found)");
+            return;
+        }
+
+        ServerInfo targetServer = ProxyServer.getInstance().getServerInfo(prevGmObj.get().Server);
+        e.setTarget(targetServer);
+
         Main.getGameModeWarpManager().sendPlayer(p, previousGameMode);
     }
 
